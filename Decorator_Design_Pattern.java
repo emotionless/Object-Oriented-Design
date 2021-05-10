11cdc6cb35a130505455db6be0de835a8cc78458
Faruk Hossain Milon <milonju01@gmail.com>
2021-05-10 18:29:18 +0600

    Decorator pattern added

---
+++ Decorator_Design_Pattern.java
@@ -0,0 +1,81 @@
+/** 
+Pattern name: Decorator Design Pattern
+Idea from:  https://medium.com/deciphering-deadlocks/what-is-decorator-design-pattern-in-java-21283d7f7c1e
+**/
+
+public abstract class Pizza {
+    String description = "Basic Pizza";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getCost();
+}
+
+public class ThickCrustPizza extends Pizza {
+    public ThickCrustPizza() {
+        description = "Thick crust Pizza, with tomato sauce";
+    }
+
+    public Double getCost() {
+        return 8.00;    // base cost of a Thick Crust Pizza
+    }
+}
+
+public class ThinCrustPizza extends Pizza {
+    public ThinCrustPizza() {
+        description = "Thin crust Pizza, with tomato sauce";
+    }
+
+    public Double getCost() {
+        return 7.50;    // base cost of a Thin Crust Pizza
+    }
+}
+
+public abstract class ToppingsDecorator extends Pizza {
+    Pizza Pizza;
+    public abstract String getDescriptionWithToppings();
+}
+
+public class Cheese extends ToppingsDecorator {
+    public Cheese(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getDescriptionWithToppings() {
+        return this.pizza.getDescription() + ", " + "cheese";
+    }
+    
+    public Double getCost() {
+        return this.pizza.getCost() + 0.50;
+    }
+}
+
+public class Olives extends Pizza {
+    public Olives(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getDescriptionWithToppings() {
+        return pizza.getDescription() + ", olives";
+    }
+
+    public Double getCost() {
+        return pizza.getCost() + 0.25;
+    }
+}
+
+
+// call Pizza from main
+
+public class PizzaStore {
+    public static void main(String args[]) {
+        Pizza pizza = new ThinCrustPizza();
+        Pizza cheesePizza = new Cheese(pizza);
+        Pizza cheesedOlivePizza = new Olives(cheesePizza);
+
+        System.out.println("CheeseOlivePizza: Description: " + cheesedOlivePizza.getDescriptionWithToppings());
+        System.out.println("CheeseOlivePizza: Cost: " + cheesedOlivePizza.getCost());
+    }
+}
